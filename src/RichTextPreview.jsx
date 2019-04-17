5a3e133e0781b58d47c79e7e032fc6e3ebc9c1b8
Rose Davidson <rose@inklesspen.com>
2019-04-17 13:41:20 -0700

    Show processing notes in rich text preview pane

--- src/RichTextPreview.jsx
+++ src/RichTextPreview.jsx
@@ -6,6 +6,7 @@ import unified from 'unified';
 import rehypeToReact from 'rehype-react';
 import rehypeDomParse from 'rehype-dom-parse';
 import styles from './RichTextPreview.module.css';
+import Note from './processing/notes';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class RichTextPreview extends Component {
@@ -15,18 +16,31 @@ class RichTextPreview extends Component {
       .use(rehypeToReact, { createElement: React.createElement });
     const result = processor.processSync(this.props.htmlValue).contents;
 
+    const notes = this.props.processingNotes
+      .map(noteName => Note.enumValueOf(noteName)).map(note => (
+        <li key={note.ordinal}>{note.short}</li>
+      ));
+
     return (
-      <div className={styles.PreviewContainer}>
-        {result}
-      </div>
+      <React.Fragment>
+        <div className={styles.PreviewContainer}>
+          {result}
+        </div>
+        <div>
+          <ul>
+            {notes}
+          </ul>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
 RichTextPreview.propTypes = {
   htmlValue: PropTypes.string.isRequired,
+  processingNotes: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
-const mapState = state => ({ htmlValue: state.htmlValue });
+const mapState = state => ({ htmlValue: state.htmlValue, processingNotes: state.processingNotes });
 
 export default connect(mapState)(RichTextPreview);
