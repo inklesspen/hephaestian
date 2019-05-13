297abf77fc509ab8d893cb343293b72ea0e753fb
Rose Davidson <rose@inklesspen.com>
2019-05-12 23:23:57 -0700

    Add reference note.

--- src/htmlparser2-utils/hast-util-from-htmlparser2.js
+++ src/htmlparser2-utils/hast-util-from-htmlparser2.js
@@ -1,6 +1,10 @@
 import { parseDOM } from 'htmlparser2';
 import domToHast from './htmlparser2-dom-to-hast';
 
+// An early version of rehype-parse worked with htmlparser2
+// https://github.com/rehypejs/rehype/blob/0dbea955ba5274e089227e5c71022ff125387888/packages/rehype-parse/lib/parser.js
+// consult if necessary
+
 const parserDefaults = {
   withStartIndices: true,
   withEndIndices: true,
